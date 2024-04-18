6215c13a19561a29d94ff3b54ad867517fe826f6
Haveadream1 <260602david@gmail.com>
2024-04-18 10:20:20 +0200

    Init setup dropdown menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Imported js from exo odin
 
+// Popup JS
 const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
@@ -18,7 +19,25 @@ const testR = () => {
 
     backDrop.classList.remove('show');
     modalDiv.classList.add('back');
+    modalDiv.classList.remove('show');
 }
 
 openButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', testR);
+
+// Dropdown JS
+const dropdownContainer = document.querySelector(".dropdown-container");
+const menuTitle = document.querySelector(".menu-title");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+menuTitle.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) {
+    dropdownMenu.classList.toggle("visible");
+  }  
+})
+
+window.addEventListener("click", (e) => {
+  if (!dropdownContainer.contains(e.target)) {
+    dropdownMenu.classList.remove("visible")
+  }
+})
